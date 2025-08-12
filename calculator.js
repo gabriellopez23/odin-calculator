175016e24c889ed4006f5d1cc74bc9e2375818a0
Gabriel Lopez Lugo <glugo3@gatech.edu>
2025-08-12 05:39:52 -0400

    Massive code reorganization and renaming

--- calculator.js
+++ calculator.js
@@ -1,3 +1,64 @@
+/**
+ * CONSTANST AND STATE
+ */
+const MAX_LEN = 15;
+const MAX_FRAC_DIGITS = 12;
+
+const State = {
+    ENTERING_LEFT:      'left',
+    OPERATOR_SELECTED:  'operator',
+    ENTERING_RIGHT:     'right',
+    RESULT_DISPLAYED:   'result',
+};
+
+let operands = {
+    left:       '',
+    operator:   '',
+    right:      '',
+};
+
+let calculatorState = State.ENTERING_LEFT;
+
+/** 
+ * UTILITIES
+*/
+const isNumber = (arg) => !isNaN(arg);
+const isOperator = (arg) => '+-*/'.includes(arg);
+
+const getSymbol = id => {
+    const idToSymbol = {
+        zero:       '0',
+        one:        '1',
+        two:        '2',
+        three:      '3',
+        four:       '4',
+        five:       '5',
+        six:        '6',
+        seven:      '7',
+        eight:      '8',
+        nine:       '9',
+        add:        '+',
+        subtract:   '-',
+        multiply:   '*',
+        divide:     '/',
+    };
+    return idToSymbol[id]
+}; 
+
+const hasDecimal = (value) => value.includes('.');
+const isZeroInteger = (value) => parseInt(value) === 0 && !hasDecimal(value);
+const isNegated = (value) => value.startsWith('-');
+const isEmpty = (value) => value.length === 0;
+
+const getTrueLength = (value) => {
+    const decimalOffset = Number(hasDecimal(value));
+    const negationOffset = Number(isNegated(value));
+    return value.length - decimalOffset - negationOffset;
+}
+
+/**
+ * CALCULATION LOGIC
+ */
 function operate(left, operator, right) {
     const operatorToOperation = {
         "+": (a, b) => Number(a) + Number(b),
@@ -7,27 +68,6 @@ function operate(left, operator, right) {
     };
     const result = operatorToOperation[operator](left, right);
     return result.toString();
-}
-
-let operands = {
-    left: '',
-    operator: '',
-    right: '',
-};
-let operandEditMode = 'left';
-
-const hasDecimal = (num) => num.includes('.');
-const isZeroInteger = (operand) => parseInt(operand) === 0 && !hasDecimal(operand);
-const isNegated = (num) => num.startsWith('-');
-const isEmpty = (num) => num.length === 0;
-
-const MAX_LEN = 15;
-const MAX_FRAC_DIGITS = 12;
-
-const getTrueLength = (value) => {
-    const decimalOffset = Number(hasDecimal(value));
-    const negationOffset = Number(isNegated(value));
-    return value.length - decimalOffset - negationOffset;
 }
 
 function updateNumber(operand, inputData) {
@@ -52,126 +92,6 @@ function updateNumber(operand, inputData) {
     }
 }
 
-function updateDisplay() {
-    const display = document.querySelector(".display");
-
-    // If edit mode is 'result', display left since it stores the result
-    const operand = operandEditMode === 'result' ? 
-                    operands['left'] : 
-                    operands[operandEditMode];
-
-    if (isEmpty(operand)){
-        display.textContent = '0';
-    } else if (getTrueLength(operand) > MAX_LEN) {
-        const value = Number(operand);
-        const expForm = value.toExponential(MAX_FRAC_DIGITS);
-        display.textContent = expForm;
-    } else {
-        display.textContent = operand;
-    }
-
-}
-
-function clearData() {
-    operands.left = '';
-    operands.operator = '';
-    operands.right = '';
-}
-
-function clearFunction() {
-    // Reset edit mode and data
-    clearData();
-    operandEditMode = 'left';
-}
-
-function clearEntry() {
-    if (operandEditMode === 'result') {
-        clearData();
-    } else {
-        operands[operandEditMode] = '';
-    }
-}
-
-function numFunction(input) {
-    switch (operandEditMode) {
-        case 'left':
-        case 'right':
-            let operand = operands[operandEditMode];
-            operands[operandEditMode] = updateNumber(operand, input);
-            break;
-        case 'operator':
-            operandEditMode = 'right';
-            operands['right'] = input;
-            break;
-        case 'result':
-            clearData();
-            operandEditMode = 'left';
-            operands['left'] = input;
-            break;
-    }
-}
-
-function opFunction(input) {
-    let {left, operator, right} = operands;
-    let result = null;
-
-    switch (operandEditMode) {
-        case 'left':
-            if (!isEmpty(left)) {
-                operandEditMode = 'operator';
-                operands['operator'] = input;
-            }
-            break;
-        case 'right':
-            operandEditMode = 'operator';
-
-            // Store result in left
-            result = operate(left, operator, right);
-            operands['left'] = result;
-
-            operands['operator'] = input;
-            break;
-        case 'operator':
-            operands['operator'] = input;
-            break;
-        case 'result':
-            operandEditMode = 'operator';
-            operands['operator'] = input;
-            break;
-    }
-}
-
-function resFunction() {
-    let {left, operator, right} = operands;
-    let result = null;
-
-    switch (operandEditMode) {
-        case 'left':
-            break;
-        case 'right':
-            operandEditMode = 'result';
-
-            // Store result in left
-            result = operate(left, operator, right);
-            operands['left'] = result;
-            break;
-        case 'operator':
-            operandEditMode = 'result';
-
-            // Store result in left
-            result = operate(left, operator, left);
-            operands['left'] = result;
-
-            // Duplicate left into right to reuse as operands
-            operands['right'] = left;
-            break;
-        case 'result':
-            result = operate(left, operator, right);
-            operands['left'] = result;
-            break;
-    }
-}
-
 function negate(operand) {
     /**
      * Negate any number, unless it is a 0 without a decimal point
@@ -180,22 +100,6 @@ function negate(operand) {
         return operand;
     } else {
         return (isNegated(operand) ? operand.substring(1) : '-' + operand);
-    }
-}
-
-function negFunction() {
-    switch (operandEditMode) {
-        case 'left':
-        case 'right':
-            let operand = operands[operandEditMode];
-            operands[operandEditMode] = negate(operand);
-            break;
-        case 'operator':
-            break;
-        case 'result':
-            operandEditMode = 'left';
-            operands['left'] = negate(operands['left']);
-            break;
     }
 }
 
@@ -211,67 +115,186 @@ function backspace(operand) {
     return operand.substring(0, operand.length - 1);
 }
 
-function delFunction() {
-    switch (operandEditMode) {
-        case 'left':
-        case 'right':
-        case 'operator':
-            let operand = operands[operandEditMode];
-            operands[operandEditMode] = backspace(operand);
+/**
+ * STATE HANDLERS
+ */
+function clearData() {
+    operands.left = '';
+    operands.operator = '';
+    operands.right = '';
+}
+
+function handleClearAll() {
+    // Reset edit mode and data
+    clearData();
+    calculatorState = State.ENTERING_LEFT;
+}
+
+function handleClearEntry() {
+    if (calculatorState === State.RESULT_DISPLAYED) {
+        clearData();
+    } else {
+        operands[calculatorState] = '';
+    }
+}
+
+function handleNumber(input) {
+    switch (calculatorState) {
+        case State.ENTERING_LEFT:
+        case State.ENTERING_RIGHT:
+            let operand = operands[calculatorState];
+            operands[calculatorState] = updateNumber(operand, input);
             break;
-        case 'result':
+        case State.OPERATOR_SELECTED:
+            calculatorState = State.ENTERING_RIGHT;
+            operands['right'] = input;
+            break;
+        case State.RESULT_DISPLAYED:
+            clearData();
+            calculatorState = State.ENTERING_LEFT;
+            operands['left'] = input;
             break;
     }
 }
 
-function decFunction() {
-    const addDecimal = (operand) => {
-        if (!hasDecimal(operands[operand])) {
-            const data = (isEmpty(operands[operand])) ? '0.' : '.';
-            operands[operand] += data;
+function handleOperator(input) {
+    let {left, operator, right} = operands;
+    let result = null;
+
+    switch (calculatorState) {
+        case State.ENTERING_LEFT:
+            if (!isEmpty(left)) {
+                calculatorState = State.OPERATOR_SELECTED;
+                operands['operator'] = input;
+            }
+            break;
+        case State.ENTERING_RIGHT:
+            calculatorState = State.OPERATOR_SELECTED;
+
+            // Store result in left
+            result = operate(left, operator, right);
+            operands['left'] = result;
+
+            operands['operator'] = input;
+            break;
+        case State.OPERATOR_SELECTED:
+            operands['operator'] = input;
+            break;
+        case State.RESULT_DISPLAYED:
+            calculatorState = State.OPERATOR_SELECTED;
+            operands['operator'] = input;
+            break;
+    }
+}
+
+function handleResult() {
+    let {left, operator, right} = operands;
+    let result = null;
+
+    switch (calculatorState) {
+        case State.ENTERING_LEFT:
+            break;
+        case State.ENTERING_RIGHT:
+            calculatorState = State.RESULT_DISPLAYED;
+
+            // Store result in left
+            result = operate(left, operator, right);
+            operands['left'] = result;
+            break;
+        case State.OPERATOR_SELECTED:
+            calculatorState = State.RESULT_DISPLAYED;
+
+            // Store result in left
+            result = operate(left, operator, left);
+            operands['left'] = result;
+
+            // Duplicate left into right to reuse as operands
+            operands['right'] = left;
+            break;
+        case State.RESULT_DISPLAYED:
+            result = operate(left, operator, right);
+            operands['left'] = result;
+            break;
+    }
+}
+
+function handleNegate() {
+    switch (calculatorState) {
+        case State.ENTERING_LEFT:
+        case State.ENTERING_RIGHT:
+            let operand = operands[calculatorState];
+            operands[calculatorState] = negate(operand);
+            break;
+        case State.OPERATOR_SELECTED:
+            break;
+        case State.RESULT_DISPLAYED:
+            calculatorState = State.ENTERING_LEFT;
+            operands['left'] = negate(operands['left']);
+            break;
+    }
+}
+
+function handleDelete() {
+    switch (calculatorState) {
+        case State.ENTERING_LEFT:
+        case State.ENTERING_RIGHT:
+        case State.OPERATOR_SELECTED:
+            let operand = operands[calculatorState];
+            operands[calculatorState] = backspace(operand);
+            break;
+        case State.RESULT_DISPLAYED:
+            break;
+    }
+}
+
+function handleDecimal() {
+    const addDecimal = mode => {
+        if (!hasDecimal(operands[mode])) {
+            const data = (isEmpty(operands[mode])) ? '0.' : '.';
+            operands[mode] += data;
         }
     }
 
-    switch (operandEditMode) {
-        case 'left':
-        case 'right':
-            addDecimal(operandEditMode);
+    switch (calculatorState) {
+        case State.ENTERING_LEFT:
+        case State.ENTERING_RIGHT:
+            addDecimal(calculatorState);
             break;
-        case 'operator':
-            operandEditMode = 'right';
+        case State.OPERATOR_SELECTED:
+            calculatorState = State.ENTERING_RIGHT;
             addDecimal('right')
             break;
-        case 'result':
+        case State.RESULT_DISPLAYED:
             clearData();
             addDecimal('left');
             break;
     }
 }
 
-const isNumber = (arg) => !isNaN(arg);
-const isOperator = (arg) => '+-*/'.includes(arg);
+/**
+ * DOM METHODS
+ */
+function updateDisplay() {
+    const display = document.querySelector(".display");
 
-const getSymbol = id => {
-    const idToSymbol = {
-        zero:       '0',
-        one:        '1',
-        two:        '2',
-        three:      '3',
-        four:       '4',
-        five:       '5',
-        six:        '6',
-        seven:      '7',
-        eight:      '8',
-        nine:       '9',
-        add:        '+',
-        subtract:   '-',
-        multiply:   '*',
-        divide:     '/',
-    };
-    return idToSymbol[id]
-}; 
+    // If edit mode is 'result', display left since it stores the result
+    const operand = calculatorState === State.RESULT_DISPLAYED ? 
+                    operands['left'] : 
+                    operands[calculatorState];
 
-function setupKeyboard() {
+    if (isEmpty(operand)){
+        display.textContent = '0';
+    } else if (getTrueLength(operand) > MAX_LEN) {
+        const value = Number(operand);
+        const expForm = value.toExponential(MAX_FRAC_DIGITS);
+        display.textContent = expForm;
+    } else {
+        display.textContent = operand;
+    }
+
+}
+
+function initializeKeyboard() {
     document.addEventListener("keydown", function (event) {
         if (event.defaultPrevented) {
             return;
@@ -279,26 +302,26 @@ function setupKeyboard() {
 
         switch (event.key) {
             case 'Escape':
-                clearFunction();
+                handleClearAll();
                 break;
             case 'Delete':
-                clearEntry();
+                handleClearEntry();
                 break;
             case 'Backspace':
-                delFunction();
+                handleDelete();
                 break;
             case '.':
-                decFunction();
+                handleDecimal();
                 break;
             case 'Enter':
             case '=':
-                resFunction();
+                handleResult();
                 break;
             default:
                 if (isNumber(event.key)) {
-                    numFunction(event.key);
+                    handleNumber(event.key);
                 } else if(isOperator(event.key)) {
-                    opFunction(event.key);
+                    handleOperator(event.key);
                 }  
                 break;
         }
@@ -306,34 +329,34 @@ function setupKeyboard() {
         }, true);
 }
 
-function setupButtons() {
+function initializeButtons() {
     const buttons = document.querySelectorAll("button");
     for (const button of buttons) {
         let id = button.id;
 
         if (id === 'clear') {
-            button.onclick = () => clearFunction();
+            button.onclick = () => handleClearAll();
         } else if (id === 'clear-entry') {
-            button.onclick = () => clearEntry();
+            button.onclick = () => handleClearEntry();
         } else if (id === 'result') {
-            button.onclick = () => resFunction();
+            button.onclick = () => handleResult();
         } else if (id === 'delete') {
-            button.onclick = () => delFunction();
+            button.onclick = () => handleDelete();
         } else if (id === 'decimal') {
-            button.onclick = () => decFunction();
+            button.onclick = () => handleDecimal();
         } else if (id === 'negate') {
-            button.onclick = () => negFunction();
+            button.onclick = () => handleNegate();
         } else if (isNumber(getSymbol(id))) {
-            button.onclick = () => numFunction(getSymbol(id));
+            button.onclick = () => handleNumber(getSymbol(id));
         } else {
-            button.onclick = () => opFunction(getSymbol(id));
+            button.onclick = () => handleOperator(getSymbol(id));
         }
 
-        // Clicking any button should update the display
+        // Clicking any button updates the display
         button.addEventListener("click", () => updateDisplay());
     }
 } 
 
-setupButtons();
-setupKeyboard();
+initializeButtons();
+initializeKeyboard();
 updateDisplay();
